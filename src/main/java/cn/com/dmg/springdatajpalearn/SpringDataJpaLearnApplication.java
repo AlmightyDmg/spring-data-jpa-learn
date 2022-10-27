f9830c5885d5be03dc7308951d28ce9bbffecab4
zhumeng <zhumeng@haizhi.com>
2022-10-27 18:13:37 +0800

    初始化代码

---
+++ src/main/java/cn/com/dmg/springdatajpalearn/SpringDataJpaLearnApplication.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.springdatajpalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/dmg/springdatajpalearn/SpringDataJpaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.springdatajpalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
